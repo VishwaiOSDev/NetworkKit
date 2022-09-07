7d9a0985bd780cda24b19cc4b766ec15691914e9
VishwaiOSDev <vishwa300499@icloud.com>
2022-09-07 10:22:07 +0530

    Added NetworkRequestable protocol for generating generic URL Endpoints.

---
+++ Sources/NetworkKit/NetworkRequestable.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkRequestable.swift
+//  
+//
+//  Created by Vishweshwaran on 07/09/22.
+//
+
+import Foundation
+
+public protocol NetworkRequestable {
+    var url: URL { get }
+    var path: String { get }
+    var httpMethod: HTTPMethod { get }
+    var queryParameter: [String: Any]? { get }
+}
