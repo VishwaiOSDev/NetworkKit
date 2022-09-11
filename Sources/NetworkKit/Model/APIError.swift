2caf4ea0a93cbcedd05a51fba5ed991bf6bda241
VishwaiOSDev <vishwa300499@icloud.com>
2022-09-11 13:40:59 +0530

    Added API Error(Generic)

---
+++ Sources/NetworkKit/Model/APIError.swift
@@ -0,0 +1,53 @@
+//
+//  APIError.swift
+//  
+//
+//  Created by Vishweshwaran on 11/09/22.
+//
+
+import Foundation
+
+public struct APIError: Error {
+    
+    public let statusCode: Int
+    public var data: Date?
+    
+    var networkError: NetworkError {
+        switch statusCode {
+        case 400: return .badInput(error: self)
+        case 404: return .notFound(error: self)
+        case 429: return .tooManyRequest(error: self)
+        case 401...449: return .authError(error: self)
+        case 500: return .internalServerError(error: self)
+        case 501...599: return .badRequest(error: self)
+        default: return .undefined(error: self)
+        }
+    }
+}
+
+public enum NetworkError: LocalizedError {
+    
+    /// Status code `400...499`
+    case authError(error: APIError)
+    case notFound(error: APIError)
+    case tooManyRequest(error: APIError)
+    case badInput(error: APIError)
+    /// Status code `500...599`
+    case badRequest(error: APIError)
+    case undefined(error: APIError)
+    case internalServerError(error: APIError)
+    case noData
+    
+    public var errorDescription: String? {
+        switch self {
+        case .authError(let error): return "[\(error.statusCode)]: authentication error"
+        case .notFound(let error): return "[\(error.statusCode)]: path which is requested not found"
+        case .tooManyRequest(let error): return "[\(error.statusCode)]: too many request"
+        case .badInput(let error): return "[\(error.statusCode)]: bad input"
+        case .badRequest(let error): return "[\(error.statusCode)]: bad request"
+        case .undefined(let error): return "[\(error.statusCode)]: undefined error"
+        case .internalServerError(let error): return "[\(error.statusCode)]: internal server error"
+        case .noData: return "no data"
+        }
+    }
+}
--- Sources/NetworkKit/NetworkKit.swift
+++ Sources/NetworkKit/NetworkKit.swift
@@ -14,29 +14,39 @@ public final class NetworkKit {
     
     private init() { }
     
-    public func request<T: Codable>(_ requestable: NetworkRequestable, type: T.Type) async throws -> T {
-        let url = requestable.url.addQueryParamIfNeeded(requestable.queryParameter)
-        let request = buildRequest(from: url, methodType: requestable.httpMethod)
+    public func requestJSON<T: Codable>(_ requestable: NetworkRequestable, type: T.Type) async throws -> T {
+        return try await processRequest(for: requestable, to: type)
+    }
+}
+
+extension NetworkKit {
+    
+    fileprivate func processRequest<T: Codable>(for request: NetworkRequestable, to type: T.Type) async throws -> T  {
+        let url = request.url.addQueryParamIfNeeded(request.queryParameter)
+        let request = buildRequest(from: url, methodType: request.httpMethod)
         do {
             let (data, response) = try await URLSession.shared.data(for: request)
             try validateResponse(response)
             let JSONResponse = try JSONDecoder().decode(T.self, from: data)
             return JSONResponse
         } catch {
-            throw NetworkingError.failedToDecode(error: error)
-        }
-    }
-    
-    private func validateResponse(_ response: URLResponse) throws {
-        guard let response = response as? HTTPURLResponse else { throw URLError(.badServerResponse) }
-        guard (200...300) ~= response.statusCode else {
-            throw NetworkingError.invaildStatusCode(statusCode: response.statusCode)
+            throw URLError(.badURL)
+//            throw NetworkingError.failedToDecode(error: error)
         }
     }
 }
 
-private extension NetworkKit {
-    func buildRequest(from url: URL, methodType: HTTPMethod) -> URLRequest {
+extension NetworkKit {
+    
+    fileprivate func validateResponse(_ response: URLResponse) throws {
+        guard let response = response as? HTTPURLResponse else { throw URLError(.badServerResponse) }
+        guard (200...299) ~= response.statusCode else {
+            throw URLError(.badURL)
+//            throw NetworkingError.invaildStatusCode(statusCode: response.statusCode)
+        }
+    }
+    
+    fileprivate func buildRequest(from url: URL, methodType: HTTPMethod) -> URLRequest {
         var request = URLRequest(url: url)
         request.httpMethod = methodType.rawValue
         return request
