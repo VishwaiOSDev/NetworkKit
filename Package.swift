ca716e01f7fef6b5d84d18e462867eaa898554fb
Vishwa <71421776+VishwaiOSDev@users.noreply.github.com>
2022-12-29 17:23:42 +0530

    Refactor: Removed SwiftLint Package Plugin

--- Package.swift
+++ Package.swift
@@ -10,19 +10,13 @@ let package = Package(
         .library(name: "NetworkKit", targets: ["NetworkKit"]),
     ],
     dependencies: [
-        .package(url: "git@github.com:VishwaiOSDev/LogKit.git", .upToNextMajor(from: "0.0.9")),
-        
-        // Plugin
-        .package(url: "https://github.com/lukepistrol/SwiftLintPlugin", from: "0.1.0")
+        .package(url: "git@github.com:VishwaiOSDev/LogKit.git", .upToNextMajor(from: "0.0.9"))
     ],
     targets: [
         .target(
             name: "NetworkKit",
             dependencies: [
                 .product(name: "LogKit", package: "LogKit")
-            ],
-            plugins: [
-                .plugin(name: "SwiftLint", package: "SwiftLintPlugin")
             ]
         ),
         .testTarget(name: "NetworkKitTests", dependencies: ["NetworkKit"]),
